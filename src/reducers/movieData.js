e7ecf4bf97eaafba53117764ecf423c146b39873
Michael <mschneider247@gmail.com>
2019-10-23 15:21:50 -0600

    Correct spelling error in movieData reducer payload

--- src/reducers/movieData.js
+++ src/reducers/movieData.js
@@ -1,7 +1,7 @@
 export const movieData = (state = [], action) => {
   switch (action.type) {
     case 'SET_MOVIES':
-      return action.errorMsg
+      return action.movies
     default:
       return state
   }
