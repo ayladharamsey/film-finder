ea20f59de1f2976efd390f9a390144e9a4dabf1d
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-23 20:24:30 -0600

    fetch all of the upcoming movies

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,10 +1,16 @@
+const apiKey = '0f1d43bea84ed1843958538d348af0d5';
+const upcomingMovies = '/popular/';
+
+
+
 export const getMovies = async () => {
-  const url = 'https://api.themoviedb.org/3/movie/550?api_key=0f1d43bea84ed1843958538d348af0d5'
+  const url = `https://api.themoviedb.org/3/movie${upcomingMovies}?api_key=${apiKey}`
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error('There was an error getting your movies!!');
   }
   const movies = await response.json();
+  console.log(movies)
   return movies
 }
 
