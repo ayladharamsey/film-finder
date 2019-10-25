a50e1431c8df5fb0355571ad100d7185cbebf179
Michael <mschneider247@gmail.com>
2019-10-24 21:32:47 -0600

    Link test to UserProfile, so far no content in container UserProfile

---
+++ src/Containers/UserProfile/UserProfile.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import UserProfile from './UserProfile';
+
+describe('UserProfile container tests', () => {
+
+  it('Should make sure the test is firing', () => {
+    expect(true).toEqual(true);
+  })
+})
