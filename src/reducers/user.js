285ab1a40db664ae6c40f1bee96aab127ac38c37
Kate Malone <katerosemalone@gmail.com>
2019-10-25 12:32:02 -0600

    Updates user reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,8 +1,20 @@
 export const user = (state = {}, action) => {
   switch (action.type) {
     case 'SET_USER':
+      console.log(action.user)
       return action.user
     default:
       return state
   }
 }
+
+
+// case 'CREATE_USER':
+//   return {
+//   name: action.name,
+//   email: action.email,
+//   password: action.password
+// }
+    
+//     default:
+// return state;
