9e36ae46eba11903d8078335dbc0489eb080dae5
Michael <mschneider247@gmail.com>
2019-10-26 13:48:01 -0600

    Logout button shows when a user is logged in

--- src/Containers/Nav/Nav.js
+++ src/Containers/Nav/Nav.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './Nav.scss';
-import { Route, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Nav = () => {
+
+export const Nav = (props) => {
+  const { user } = props;
+  const loginLink = <Link to='/login'><button>Login</button></Link>;
+  const logoutLink = <Link to='/'><button>Logout</button></Link>
 
   return (
     <nav>
       <h1>Movie Tracker</h1>
-      <Link to='/login'><button>Login</button></Link>
+      {user.name ? logoutLink : loginLink}
     </nav>
   )
 }
 
-export default Nav;
+export const mapStateToProps = state => ({
+  user: state.user
+});
+
+export default connect(mapStateToProps, null)(Nav);
