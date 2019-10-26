5fa8c928814740dd912b39823e42f775d5b8ef08
Michael <mschneider247@gmail.com>
2019-10-26 17:30:55 -0600

    Allow error checking when a user tries to log in
    co-authored-by: Ayla Dharamsey ayla.dharamsey@gmail.com

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -10,7 +10,9 @@ export class Login extends Component {
     super();
     this.state = {
       email: '',
-      password: '', 
+      password: '',
+      buttonClick: false,
+      error: 'error', 
     }
   }
 
@@ -19,7 +21,8 @@ export class Login extends Component {
   }
 
   handleClick = e => {
-    // e.preventDefault();
+    e.preventDefault()
+    this.setState({buttonClick: true})
     this.props.loginUser({
       email: this.state.email,
       password: this.state.password
@@ -34,8 +37,37 @@ export class Login extends Component {
     });
   }
 
+  handleError = () => {
+    return (
+      <h3>{this.props.hasErrored}</h3>
+    )
+  }
+
+  linkToMovies = () => {
+    if (this.props.user.email !== ""){
+      return (
+        <Redirect to='/movies'>Movies Link</Redirect>
+      )
+    }
+  }
+
   render() {
-    const { email, password } = this.state;
+    const { email, password, buttonClick } = this.state;
+    const { hasErrored, user } = this.props;
+    let error;
+
+    if ((buttonClick === true) && (hasErrored !== "")) {
+      error = this.handleError()
+    }
+
+    if ((buttonClick === true) && (user.id !== undefined)) {
+      error = this.linkToMovies()
+    }
+
+
+
+    // const something = hasErrored !== "" ? this.handleError() : null
+
     return (
         <form className="login_form">
           <input
@@ -50,17 +82,21 @@ export class Login extends Component {
             placeholder="Password"
             onChange={this.updateUser}
           />
-          <Link to='/movies'>
-            <button onClick={this.handleClick}>Sign in</button>
-          </Link>
+          <button onClick={(e) => this.handleClick(e)}>Sign in</button>
           <Link to='/create-user'><h5>Not a user? Create a new user account!</h5></Link>
+          {error}
         </form>
     )
   }
 }
 
+export const mapStateToProps = state => ({
+  hasErrored: state.hasErrored,
+  user: state.user,
+})
+
 export const mapDispatchToProps = dispatch => ({
   setUser: user => dispatch(setUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
