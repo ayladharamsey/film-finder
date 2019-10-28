43c14604c4791c9c0abc02d87c7f6583ce997754
Michael <mschneider247@gmail.com>
2019-10-28 12:57:47 -0600

    Change MovieCard to a class component so it can keep track of its button clicks

--- src/Containers/MovieCard/MovieCard.js
+++ src/Containers/MovieCard/MovieCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './MovieCard.scss';
 import { faveMovie } from '../../actions/index'
@@ -6,29 +6,32 @@ import favorite from '../../images/favorite.svg'
 import unfavorite from '../../images/unfavorite.svg'
 import { Redirect } from 'react-router-dom'; 
 
-export const MovieCard = (props) => {
+export class MovieCard extends Component {
+  constructor(){
+    super()
+    this.state = {
+      btnClick: false,
+    }
+  }
 
-  const favoriteMovie = (event, id) => {
-    // needs to also update the api here...
-    // I feel like in normal React this would live in app
-    // so how do I structure it for Redux?
-    // it becomes an action that goes through the reducer??
-    // that is where I'm getting confused
-    // but I want this function to be available anywhere the movies are
-    //  so here...   and eventually in MoviesInfo
-    // not sure how MoviesInfo will be structured yet....  so lets just do it here?
-    // make an api call
-    // check that they are logged in first
-    if (props.user.id === undefined) {
+  favoriteMovie = (event, id) => {
+    this.props.faveMovie(parseInt(event.target.parentNode.id))
+  }
+
+  checkLogginStatus = (event, id) => {
+    if (this.props.user.id === undefined) {
       return (
         <Redirect to='/login'>Not Logged In</Redirect>
-      )}
-    props.faveMovie(parseInt(event.target.parentNode.id))
+      )
+    }
+    this.favoriteMovie(event, id)
+    return
   }
-  
 
-  const { 
-    id, 
+  
+  
+  render() {
+    const { id, 
     movieRating, 
     backgroundImage, 
     overview, 
@@ -36,9 +39,9 @@ export const MovieCard = (props) => {
     title, 
     releaseDate, 
     isFavorited 
-  } = props;
-  let error;
-  return (
+    } = this.props;
+    
+    return (
     <article id={id} className="movie-card">
       <h1>{title}</h1>
       <p>{movieRating}</p>
@@ -47,11 +50,11 @@ export const MovieCard = (props) => {
       <p>{posterImage}</p>
       <p>{releaseDate}</p>
       {isFavorited ? 
-        <img src={favorite} alt="favorited" onClick={(event) => favoriteMovie(event, id)}/> : 
-        <img src={unfavorite} alt="un favorited" onClick={(event) => favoriteMovie(event, id)}/>}
-      {error}
+        <img src={favorite} alt="favorited" onClick={(event) => this.checkLogginStatus(event, id)}/> : 
+        <img src={unfavorite} alt="un favorited" onClick={(event) => this.checkLogginStatus(event, id)}/>}
     </article>
-  )
+    )
+  }
 }
 
 export const mapDispatchToProps = (dispatch) => ({
