847e64da54704927a275dfdbc753f7e9c5b5f9eb
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-29 13:16:13 -0600

    changes made

--- src/reducers/setFaves.js
+++ src/reducers/setFaves.js
@@ -1,7 +1,8 @@
 export const favorites = (state = [], action ) => {
     switch(action.type) {
         case 'SET_FAVES' :
-            action.faves.forEach(fave => state.push(fave))
+            console.log(action.faves)
+            // if(state.forEach(movie => movie_id === ))
         default:
             return state
     }
