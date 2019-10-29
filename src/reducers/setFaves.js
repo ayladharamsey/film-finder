6ea8dad5556b192e6891ed6f2ad90d0f30bafa25
Ayla Dharamsey <ayla.dharamsey@gmail.com>
2019-10-29 14:56:30 -0600

    lint setFaves

--- src/reducers/setFaves.js
+++ src/reducers/setFaves.js
@@ -1,8 +1,8 @@
 export const favorites = (state = [], action ) => {
-    switch(action.type) {
-        case 'SET_FAVES' :
-            return action.faves
-        default:
-            return state
-    }
+  switch(action.type) {
+    case 'SET_FAVES' :
+      return action.faves
+    default:
+      return state
+  }
 }
