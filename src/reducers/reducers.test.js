cf194488648c48fb1abbc83edccc241fb7d6c401
Michael <mschneider247@gmail.com>
2019-10-30 18:00:02 -0600

    Check that loading reducer has an initial state and that it updates its boolean

--- src/reducers/reducers.test.js
+++ src/reducers/reducers.test.js
@@ -1,5 +1,6 @@
 import { movieData } from './movieData';
 import { hasErrored } from './errorMsg';
+import { loading } from './loading';
 
 describe('movie Data Reducer', () => {
   it('Should return the initial state', () => {
@@ -12,15 +13,15 @@ describe('movie Data Reducer', () => {
     const mockMovieData = [{id: 4, title: "A Movie"}, {id: 6, title: "Another Movie"}];
     const mockAction = {type: 'SET_MOVIES', movies: mockMovieData};
     const result = movieData([], mockAction);
-    expect(result).toEqual(mockMovieData)
+    expect(result).toEqual(mockMovieData);
   });
 
   it ('Should favorite a movie when passed its id', () => {
     const mockMovieData = [{id: 4, title: "A Movie", isFavorited: false}];
     const mockSetMovies = {type: 'SET_MOVIES', movies: mockMovieData};
-    const mockState = movieData([], mockSetMovies)
+    const mockState = movieData([], mockSetMovies);
     const mockAction = {type: "FAVE_MOVIE", id: 4};
-    const result = movieData(mockState, mockAction)
+    const result = movieData(mockState, mockAction);
     expect(result).toEqual([{id: 4, title: "A Movie", isFavorited: true}]);
   })
 });
@@ -31,12 +32,30 @@ describe('errorMsg reducer', () => {
     const expected = '';
     const result = hasErrored(undefined, {});
     expect(result).toEqual(expected);
-  })
+  });
 
   it('should return error message when case is HAS_ERRORED', () => {
     const mockErrorAction = {type: 'HAS_ERRORED', error: 'Error Message'}
-    const result = hasErrored('', mockErrorAction)
-    expect(result).toEqual('Error Message')
-  })
-})
+    const result = hasErrored('', mockErrorAction);
+    expect(result).toEqual('Error Message');
+  });
+});
+
+describe('loading reducer', () => {
+
+  it('should return an intial state of true', () => {
+    const expected = true;
+    const result = loading(undefined, {});
+    expect(result).toEqual(expected);
+  });
+
+  it('should change the loading bool from false to true when case is IS_LOADING', () => {
+    const mockLoadingAction = {
+      type: 'IS_LOADING',
+      bool: false
+    }
+    const result = loading(true, mockLoadingAction);
+    expect(result).toEqual(false);
+  });
+});
 
